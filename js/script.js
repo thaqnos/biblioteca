650a27d00a2125f41adeb97924e5ba8a30aae143
thaqnos <thaqnos@gmail.com>
2019-09-27 12:04:25 -0300

    Adiciona programa que adiciona através de um array

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const livros = {
+    1: {
+        "titulo": "Javascript Eloquente",
+        "quemEscreveu": "Marijn Haverbeke",
+        "link": "https://github.com/braziljs/eloquente-javascript"
+    },
+    2: {
+        "titulo": "Você não sabe JS",
+        "quemEscreveu": "Kyle Simpson",
+        "link": "https://github.com/cezaraugusto/You-Dont-Know-JS"
+    },
+    3: {
+        "titulo": "Calibã e a Bruxa: mulheres, corpo e acumulação primitiva",
+        "quemEscreveu": "Silvia Federici",
+        "link": "http://coletivosycorax.org/wp-content/uploads/2019/09/CALIBA_E_A_BRUXA_WEB-1.pdf"
+    }
+}
+
+const ul = document.getElementById("lista-do-catalogo")
+
+for (let propriedade in livros){ // para cada propriedade em meu objeto:
+    const li = document.createElement("li") // cria elemento <li>
+    ul.appendChild(li) // linka filho na mãe
+    li.classList.add("livro") // adiciona classe
+
+    const tituloLivro = document.createElement("h2") // cria elemento <h2>
+    li.appendChild(tituloLivro) // linka o filho na mãe 
+    tituloLivro.textContent = livros[propriedade]["titulo"] //cria conteudo, chamando o objeto
+    tituloLivro.classList.add("livro__titulo") // adiciona classe (e se repete nos passos abaixo)
+   
+    const nomeAutor = document.createElement("p")
+    li.appendChild(nomeAutor)
+    nomeAutor.textContent = livros[propriedade]["quemEscreveu"]
+    nomeAutor.classList.add("livro__autoria")
+    
+    const linkLivro = document.createElement("a")
+    li.appendChild(linkLivro)
+    linkLivro.setAttribute("href", livros[propriedade]["link"])
+    linkLivro.setAttribute("target", "_blank")
+    linkLivro.textContent = "Leia aqui"
+    linkLivro.classList.add("livro__link")
+
+    const botao = document.createElement("button")
+    li.appendChild(botao)
+    botao.setAttribute("type", "submit")
+    botao.textContent = "Já li"
+    botao.classList.add("botao-lido")
+    
+    botao.addEventListener("click", function(){
+          tituloLivro.classList.add("livro__titulo--lido") // quando clica no meu botão, adiciono classe
+    })
+
+    const botaoDesfazer = document.createElement("button")
+    li.appendChild(botaoDesfazer)
+    botaoDesfazer.setAttribute("type", "submit")
+    botaoDesfazer.textContent = "Desfazer"
+    botaoDesfazer.classList.add("botao-desfazer")
+
+    botaoDesfazer.addEventListener("click", function(){
+        tituloLivro.classList.remove("livro__titulo--lido") // quando clico em 'desfazer', removo classe
+    })
+}
